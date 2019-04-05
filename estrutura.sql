c4eaf1e458683392f47007243f1e10328462aab9
Romualdo Fernandes <romualdofernandes@gmail.com>
2019-04-05 12:52:41 -0300

    Addt

--- estrutura.sql
+++ estrutura.sql
@@ -59,6 +59,7 @@ CREATE TABLE IF NOT EXISTS `mensagens` (
   `data_atualizacao` datetime NOT NULL,
   `obs` varchar(500) COLLATE utf8_bin NOT NULL,
   `url` text COLLATE utf8_bin NOT NULL,
+  `anexo` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=18 ;
 
